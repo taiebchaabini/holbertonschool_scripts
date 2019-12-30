10d65b745e57365f92b440ad91129d1c15770b50
Taieb Chaabini <taiebchaabini@gmail.com>
2019-12-30 16:06:57 +0000

    First commit

---
+++ README.md
@@ -0,0 +1,8 @@
+## Scripts to prepare environment for HolbertonSchool Studies
+
+# holbertonschool.sh includes:
+- Git Installation
+- Betty Coding Style 
+- C Indentation (.vimrc) file configuration
+- Python 3 && PEP 1.7.4
+- Generating SSH KEY for your github account, and showing the result in terminal.
---
+++ holbertonschool.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+sudo apt-get update
+sudo apt-get upgrade
+## Git install
+sudo apt-get install git -y
+## Betty Coding Style
+git clone https://github.com/holbertonschool/Betty.git
+sudo Betty/install.sh
+rm -Rf Betty
+rm ~/.vimrc
+## C indentation
+echo "set tabstop=8 shiftwidth=8" >> ~/.vimrc
+echo "set autoindent" >> ~/.vimrc
+echo "set smartindent" >> ~/.vimrc
+echo "set cindent" >> ~/.vimrc
+echo "syntax enable" >> ~/.vimrc
+echo "set number" >> ~/.vimrc
+# Python3 && PEP8
+sudo apt-get install python3-pep8 -y
+sudo apt-get install python3-pip -y
+sudo apt-get install pep8 -y
+sudo rm -Rf /usr/lib/python3*/dist-packages/pep*
+sudo pip3 install pep8
+sudo cp /usr/local/lib/python3*/dist-packages/pep8.py /usr/bin/pep8
+sudo chmod u+x /usr/bin/pep8
+# Generate SSH KEY for github
+ssh-keygen -t rsa -b 4096 -C "taiebchaabini@gmail.com" -f $HOME/.ssh/id_rsa
+cat $HOME/.ssh/id_rsa.pub
+git config --global user.email "taiebchaabini@gmail.com"
+git config --global user.name "Taieb Chaabini"
